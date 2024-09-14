f07b4cad0fe52746f30d90b0ea5d5e78239695e7
Harley Santos Garzón <harley.santos.garzon@gmail.com>
2024-09-14 13:16:27 -0500

    feat(web|root): add page routes

--- projects/www/src/app/app.routes.ts
+++ projects/www/src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
+import { LayoutComponent } from './layout/main/layout.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  component: LayoutComponent,
+  children: [
+    {
+      path: '',
+      loadChildren: () => import('./pages/home/home.routes').then(m => m.HomeRouting)
+    },
+    {
+      path: 'demo',
+      loadChildren: () => import('./pages/demo/demo.routes').then(m => m.DemoRouting)
+    },
+    {
+      path: 'products',
+      loadChildren: () => import('./pages/products/products.routes').then(m => m.ProductsRouting)
+    },
+  ]
+}]
